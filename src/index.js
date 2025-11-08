5f0bc757afa8533d55a382bd86526975ec8fa999
Shane <6071159+smashedr@users.noreply.github.com>
2025-11-07 17:32:46 -0800

    Updates and node24

--- src/index.js
+++ src/index.js
@@ -1,32 +1,37 @@
 const core = require('@actions/core')
-const github = require('@actions/github')
 
-;(async () => {
-    try {
-        console.log('github.context.issue:', github.context.issue)
+async function main() {
+    const version = process.env.GITHUB_ACTION_REF
+        ? `${process.env.GITHUB_ACTION_REF}`
+        : 'Source'
+    core.info(`🏳️ Starting Parse Issue Form Action - \u001b[35;1m${version}`)
 
-        const body = core.getInput('body', { required: true })
-        console.log('body:', body)
+    const body = core.getInput('body', { required: true })
+    core.startGroup('Issue Body')
+    console.log(body)
+    core.endGroup() // body
 
-        const parsed = parseData(body)
-        console.log('parsed:', parsed)
+    const parsed = parseData(body)
+    console.log('parsed:', parsed)
 
-        const outputs = []
-        for (let [key, value] of Object.entries(parsed)) {
-            key = key.replace(' ', '_').toLowerCase()
-            console.log(`set output: ${key}:`, JSON.stringify(value))
-            core.setOutput(key, value)
-        }
-        console.log('created outputs:', outputs)
-
-        core.info(`\u001b[32;1mFinished Success`)
-    } catch (e) {
-        core.debug(e)
-        core.info(e.message)
-        core.setFailed(e.message)
+    const outputs = []
+    for (let [key, value] of Object.entries(parsed)) {
+        key = key.replaceAll(' ', '_').toLowerCase()
+        core.startGroup(`Set Output: ${key}`)
+        console.log(JSON.stringify(value))
+        core.endGroup() // output
+        core.setOutput(key, value)
+        outputs.push(key)
     }
-})()
+    console.log('Created Outputs:', outputs)
 
+    core.info('✅ \u001b[32;1mFinished Success')
+}
+
+/**
+ * @param {string} input
+ * @return {object}
+ */
 function parseData(input) {
     const lines = input.split('\n')
     const data = {}
@@ -51,3 +56,9 @@ function parseData(input) {
     }
     return data
 }
+
+main().catch((e) => {
+    core.debug(e)
+    core.info(e.message)
+    core.setFailed(e.message)
+})
